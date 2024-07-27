835b0e6df6bbbf8bfc56fb9afc9c3b4423869af4
Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>
2024-07-27 17:32:10 +0200

    preparing final versions of LSTM & GRU models
    Signed-off-by: Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>

---
+++ code/future_position_prediction/GRU/PosVelAcc/GRULightningModelConcat.py
@@ -0,0 +1,270 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import lightning as L
+from typing import Tuple
+
+from utils import (
+    compute_ADE,
+    compute_FDE,
+    compute_AIOU,
+    compute_FIOU,
+    convert_velocity_to_positions,
+)
+
+from GRUNet.DecoderGRU import DecoderGRU
+from GRUNet.EncoderGRU import EncoderGRU
+
+
+class GRULightningModelConcat(L.LightningModule):
+    def __init__(
+        self,
+        lr: float = 1e-4,
+        input_frames: int = 10,
+        output_frames: int = 10,
+        batch_size: int = 32,
+        bbox_dim: int = 4,
+        hidden_dim: int = 256,
+        dropout: float = 0.1,
+        image_size: Tuple[int, int] = (640, 480),
+        encoder_layer_nb: int = 1,
+        decoder_layer_nb: int = 1,
+        scheduler_patience: int = 5,
+        scheduler_factor: float = 0.5,
+    ):
+        super(GRULightningModelConcat, self).__init__()
+        self.save_hyperparameters()
+
+        self.bbox_encoder = EncoderGRU(
+            input_dim=bbox_dim,
+            hidden_dim=hidden_dim,
+            n_layers=encoder_layer_nb,
+            output_frames_nb=output_frames,
+            dropout=dropout,
+        )
+        self.vel_encoder = EncoderGRU(
+            input_dim=bbox_dim,
+            hidden_dim=hidden_dim,
+            n_layers=encoder_layer_nb,
+            output_frames_nb=output_frames,
+            dropout=dropout,
+        )
+        self.acc_encoder = EncoderGRU(
+            input_dim=bbox_dim,
+            hidden_dim=hidden_dim,
+            n_layers=encoder_layer_nb,
+            output_frames_nb=output_frames,
+            dropout=dropout,
+        )
+
+        self.pos_decoder = DecoderGRU(
+            bbox_dim,
+            hidden_dim,
+            bbox_dim,
+            n_layers=decoder_layer_nb,
+            dropout=[dropout, dropout],
+        )
+        self.vel_decoder = DecoderGRU(
+            bbox_dim,
+            hidden_dim,
+            bbox_dim,
+            n_layers=decoder_layer_nb,
+            dropout=[dropout, dropout],
+        )
+
+        self.combine_hidden = nn.Linear(hidden_dim * 3, hidden_dim)
+
+    def combine_hidden_states(self, hidden_bbox, hidden_velocity, hidden_acceleration):
+        combined = torch.cat(
+            (hidden_bbox, hidden_velocity, hidden_acceleration), dim=-1
+        )
+        return self.combine_hidden(combined)
+
+    def forward(
+        self,
+        bbox_seq,
+        velocity_seq,
+        acceleration_seq,
+    ):
+        batch_size = bbox_seq.size(0)
+
+        # Initialize hidden state
+        h_pos = torch.zeros(
+            self.bbox_encoder.n_layers,
+            batch_size,
+            self.hparams.hidden_dim,
+            device=bbox_seq.device,
+        )
+        h_vel = torch.zeros(
+            self.vel_encoder.n_layers,
+            batch_size,
+            self.hparams.hidden_dim,
+            device=velocity_seq.device,
+        )
+        h_acc = torch.zeros(
+            self.acc_encoder.n_layers,
+            batch_size,
+            self.hparams.hidden_dim,
+            device=acceleration_seq.device,
+        )
+
+        # Encode the Position, Velocity, and Acceleration
+        _, encoder_hidden_states_bbox = self.bbox_encoder(bbox_seq, h_pos)
+        _, encoder_hidden_states_vel = self.vel_encoder(velocity_seq, h_vel)
+        _, encoder_hidden_states_acc = self.acc_encoder(acceleration_seq, h_acc)
+
+        # Combine hidden states
+        h_pos = h_vel = self.combine_hidden_states(
+            encoder_hidden_states_bbox,
+            encoder_hidden_states_vel,
+            encoder_hidden_states_acc,
+        )
+
+        # decoder with teacher forcing
+        decoder_input_pos = bbox_seq[:, -1, :]
+        decoder_input_vel = velocity_seq[:, -1, :]
+
+        predictions_position = torch.tensor([], device=bbox_seq.device)
+        predictions_velocity = torch.tensor([], device=velocity_seq.device)
+
+        for t in range(self.hparams.output_frames):
+            out_pos, h_pos = self.pos_decoder(decoder_input_pos, h_pos)
+            out_vel, h_vel = self.vel_decoder(decoder_input_vel, h_vel)
+
+            predictions_position = torch.cat(
+                (predictions_position, out_pos.unsqueeze(1)), dim=1
+            )
+            predictions_velocity = torch.cat(
+                (predictions_velocity, out_vel.unsqueeze(1)), dim=1
+            )
+
+            decoder_input_pos = out_pos.detach()
+            decoder_input_vel = out_vel.detach()
+
+        return predictions_position, predictions_velocity
+
+    def _shared_step(self, batch, batch_idx, stage):
+        (
+            video_id,
+            input_bboxes,
+            input_velocities,
+            input_accelerations,
+            output_bboxes,
+            output_velocities,
+        ) = batch
+
+        # Predict future velocities
+        predicted_positions, predicted_velocity = self(
+            input_bboxes,
+            input_velocities,
+            input_accelerations,
+        )
+
+        # Convert predicted future velocities to future positions
+        velocities_to_positions = convert_velocity_to_positions(
+            predicted_velocity=predicted_velocity,
+            past_positions=input_bboxes,
+        )
+
+        # Compute losses
+        velocity_loss = F.smooth_l1_loss(predicted_velocity, output_velocities)
+        pos_loss = F.smooth_l1_loss(predicted_positions, output_bboxes)
+        velocities_to_positions_loss = F.smooth_l1_loss(
+            velocities_to_positions, output_bboxes
+        )
+        total_loss = velocity_loss + velocities_to_positions_loss * 0.1 + pos_loss
+
+        # Compute metrics
+        ade_from_vel = compute_ADE(
+            velocities_to_positions, output_bboxes, self.hparams.image_size
+        )
+        fde_from_vel = compute_FDE(
+            velocities_to_positions, output_bboxes, self.hparams.image_size
+        )
+        aiou_from_vel = compute_AIOU(velocities_to_positions, output_bboxes)
+        fiou_from_vel = compute_FIOU(velocities_to_positions, output_bboxes)
+
+        ade = compute_ADE(predicted_positions, output_bboxes, self.hparams.image_size)
+        fde = compute_FDE(predicted_positions, output_bboxes, self.hparams.image_size)
+        aiou = compute_AIOU(predicted_positions, output_bboxes)
+        fiou = compute_FIOU(predicted_positions, output_bboxes)
+
+        # Log metrics
+        self.log(
+            f"{stage}_loss", total_loss, on_step=False, on_epoch=True, prog_bar=False
+        )
+        self.log(f"{stage}_ADE", ade, on_step=False, on_epoch=True, prog_bar=False)
+        self.log(f"{stage}_FDE", fde, on_step=False, on_epoch=True, prog_bar=False)
+        self.log(
+            f"{stage}_AIoU",
+            aiou,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=False if stage == "train" else True,
+        )
+        self.log(f"{stage}_FIoU", fiou, on_step=False, on_epoch=True, prog_bar=True)
+        self.log(
+            f"{stage}_ADE_from_vel",
+            ade_from_vel,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=False,
+        )
+        self.log(
+            f"{stage}_FDE_from_vel",
+            fde_from_vel,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=False,
+        )
+        self.log(
+            f"{stage}_AIoU_from_vel",
+            aiou_from_vel,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=False if stage == "train" else True,
+        )
+        self.log(
+            f"{stage}_FIoU_from_vel",
+            fiou_from_vel,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+        )
+
+        return {
+            "total_loss": total_loss,
+            "velocity_loss": velocity_loss,
+            "velocities_to_positions_loss": velocities_to_positions_loss,
+            "ADE": ade,
+            "FDE": fde,
+            "AIoU": aiou,
+            "FIoU": fiou,
+        }
+
+    def training_step(self, batch, batch_idx):
+        return self._shared_step(batch, batch_idx, "train")["total_loss"]
+
+    def validation_step(self, batch, batch_idx):
+        return self._shared_step(batch, batch_idx, "val")
+
+    def test_step(self, batch, batch_idx):
+        return self._shared_step(batch, batch_idx, "test")
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer,
+            mode="min",
+            patience=self.hparams.scheduler_patience,
+            factor=self.hparams.scheduler_factor,
+            threshold=1e-8,
+        )
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "monitor": "val_loss",
+                "interval": "epoch",
+            },
+        }
---
+++ code/future_position_prediction/GRU/PosVelAcc/LSTMLightningModelConcat.py
@@ -0,0 +1,374 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import lightning as L
+from typing import Tuple
+import random
+
+from utils import (
+    compute_ADE,
+    compute_FDE,
+    compute_AIOU,
+    compute_FIOU,
+    convert_velocity_to_positions,
+)
+
+from LSTMEncoder import LSTMEncoder
+from LSTMDecoder import LSTMDecoder
+
+
+class LSTMLightningModelConcat(L.LightningModule):
+    def __init__(
+        self,
+        lr: float = 1e-4,
+        input_frames: int = 10,
+        output_frames: int = 10,
+        batch_size: int = 32,
+        bbox_size: int = 4,
+        hidden_size: int = 256,
+        hidden_depth: int = 3,
+        dropout: float = 0.1,
+        image_size: Tuple[int, int] = (640, 480),
+        scheduler_patience: int = 10,
+        scheduler_factor: float = 0.9,
+    ):
+        super(LSTMLightningModelConcat, self).__init__()
+        self.save_hyperparameters()
+
+        # Define LSTM Encoders
+        self.position_encoder = LSTMEncoder(
+            input_size=bbox_size,
+            hidden_size=hidden_size,
+            num_layers=hidden_depth,
+        )
+        self.velocity_encoder = LSTMEncoder(
+            input_size=bbox_size, hidden_size=hidden_size, num_layers=hidden_depth
+        )
+        self.acceleration_encoder = LSTMEncoder(
+            input_size=bbox_size, hidden_size=hidden_size, num_layers=hidden_depth
+        )
+
+        # Define LSTM Decoders
+        self.decoder_velocity = LSTMDecoder(
+            input_size=bbox_size,
+            hidden_size=hidden_size,
+            output_size=bbox_size,
+            num_layers=hidden_depth,
+            dropout=dropout,
+        )
+        self.decoder_position = LSTMDecoder(
+            input_size=bbox_size,
+            hidden_size=hidden_size,
+            output_size=bbox_size,
+            num_layers=hidden_depth,
+            dropout=dropout,
+        )
+
+        self.combine_hidden = nn.Linear(hidden_size * 3, hidden_size)
+        self.combine_cell = nn.Linear(hidden_size * 3, hidden_size)
+
+    def combine_hidden_states(self, hidden_bbox, hidden_velocity, hidden_acceleration):
+        combined = torch.cat(
+            (hidden_bbox, hidden_velocity, hidden_acceleration), dim=-1
+        )
+        return self.combine_hidden(combined)
+
+    def combine_cell_states(self, cell_bbox, cell_velocity, cell_acceleration):
+        combined = torch.cat((cell_bbox, cell_velocity, cell_acceleration), dim=-1)
+        return self.combine_cell(combined)
+
+    def forward(
+        self,
+        bbox_seq,
+        velocity_seq,
+        acceleration_seq,
+        teacher_forcing_ratio=0.5,
+        training_prediction="recursive",
+        position_seq_target=None,
+        velocity_seq_target=None,
+    ):
+        # Encode the Position, Velocity, and Acceleration
+        _, (encoder_hidden_states_bbox, encoder_cell_states_bbox) = (
+            self.position_encoder(bbox_seq)
+        )
+        _, (
+            encoder_hidden_states_vel,
+            encoder_cell_states_vel,
+        ) = self.velocity_encoder(velocity_seq)
+        _, (
+            encoder_hidden_states_acc,
+            encoder_cell_states_acc,
+        ) = self.acceleration_encoder(acceleration_seq)
+
+        # Combine the outputs and states
+        hidden_states_pos = hidden_states_vel = self.combine_hidden_states(
+            encoder_hidden_states_bbox,
+            encoder_hidden_states_vel,
+            encoder_hidden_states_acc,
+        )
+        cell_states_pos = cell_states_vel = self.combine_cell_states(
+            encoder_cell_states_bbox, encoder_cell_states_vel, encoder_cell_states_acc
+        )
+
+        # decoder with teacher forcing
+        decoder_input_pos = bbox_seq[:, -1, :]
+        decoder_input_vel = velocity_seq[:, -1, :]
+
+        predictions_position = torch.tensor([], device=bbox_seq.device)
+        predictions_velocity = torch.tensor([], device=velocity_seq.device)
+
+        if training_prediction == "recursive":
+            # predict recursively
+            for _ in range(self.hparams.output_frames):
+                decoder_output_position, (
+                    hidden_states_pos,
+                    cell_states_pos,
+                ) = self.decoder_position(
+                    decoder_input_pos, hidden_states_pos, cell_states_pos
+                )
+                decoder_output_velocity, (
+                    hidden_states_vel,
+                    cell_states_vel,
+                ) = self.decoder_velocity(
+                    decoder_input_vel, hidden_states_vel, cell_states_vel
+                )
+
+                predictions_position = torch.cat(
+                    (predictions_position, decoder_output_position.unsqueeze(1)), dim=1
+                )
+                predictions_velocity = torch.cat(
+                    (predictions_velocity, decoder_output_velocity.unsqueeze(1)), dim=1
+                )
+
+                decoder_input_pos = decoder_output_position.detach()
+                decoder_input_vel = decoder_output_velocity.detach()
+
+        if training_prediction == "teacher_forcing":
+            # use teacher forcing
+            if random.random() < teacher_forcing_ratio:
+                for t in range(self.hparams.output_frames):
+                    decoder_output_position, (
+                        hidden_states_pos,
+                        cell_states_pos,
+                    ) = self.decoder_position(
+                        decoder_input_pos, hidden_states_pos, cell_states_pos
+                    )
+                    decoder_output_velocity, (
+                        hidden_states_vel,
+                        cell_states_vel,
+                    ) = self.decoder_velocity(
+                        decoder_input_vel, hidden_states_vel, cell_states_vel
+                    )
+
+                    predictions_position = torch.cat(
+                        (predictions_position, decoder_output_position.unsqueeze(1)),
+                        dim=1,
+                    )
+                    predictions_velocity = torch.cat(
+                        (predictions_velocity, decoder_output_velocity.unsqueeze(1)),
+                        dim=1,
+                    )
+
+                    decoder_input_pos = position_seq_target[:, t, :]
+                    decoder_input_vel = velocity_seq_target[:, t, :]
+
+            # predict recursively
+            else:
+                for t in range(self.hparams.output_frames):
+                    decoder_output_position, (
+                        hidden_states_pos,
+                        cell_states_pos,
+                    ) = self.decoder_position(
+                        decoder_input_pos, hidden_states_pos, cell_states_pos
+                    )
+                    decoder_output_velocity, (
+                        hidden_states_vel,
+                        cell_states_vel,
+                    ) = self.decoder_velocity(
+                        decoder_input_vel, hidden_states_vel, cell_states_vel
+                    )
+
+                    predictions_position = torch.cat(
+                        (predictions_position, decoder_output_position.unsqueeze(1)),
+                        dim=1,
+                    )
+                    predictions_velocity = torch.cat(
+                        (predictions_velocity, decoder_output_velocity.unsqueeze(1)),
+                        dim=1,
+                    )
+
+                    decoder_input_pos = decoder_output_position.detach()
+                    decoder_input_vel = decoder_output_velocity.detach()
+
+        if training_prediction == "mixed_teacher_forcing":
+            # predict using mixed teacher forcing
+            for t in range(self.hparams.output_frames):
+                decoder_output_position, (
+                    hidden_states_pos,
+                    cell_states_pos,
+                ) = self.decoder_position(
+                    decoder_input_pos, hidden_states_pos, cell_states_pos
+                )
+                decoder_output_velocity, (
+                    hidden_states_vel,
+                    cell_states_vel,
+                ) = self.decoder_velocity(
+                    decoder_input_vel, hidden_states_vel, cell_states_vel
+                )
+
+                predictions_position = torch.cat(
+                    (predictions_position, decoder_output_position.unsqueeze(1)), dim=1
+                )
+                predictions_velocity = torch.cat(
+                    (predictions_velocity, decoder_output_velocity.unsqueeze(1)), dim=1
+                )
+
+                decoder_input_pos = decoder_output_position.detach()
+                decoder_input_vel = decoder_output_velocity.detach()
+
+                # predict with teacher forcing
+                if random.random() < teacher_forcing_ratio:
+                    decoder_input_pos = position_seq_target[:, t, :]
+                    decoder_input_vel = velocity_seq_target[:, t, :]
+
+                # predict recursively
+                else:
+                    decoder_input_pos = decoder_output_position
+                    decoder_input_vel = decoder_output_velocity
+
+        return predictions_position, predictions_velocity
+
+    def _shared_step(self, batch, batch_idx, stage):
+        (
+            video_id,
+            input_bboxes,
+            input_velocities,
+            input_accelerations,
+            output_bboxes,
+            output_velocities,
+        ) = batch
+
+        # Predict future velocities
+        if stage == "train":
+            predicted_positions, predicted_velocity = self(
+                input_bboxes,
+                input_velocities,
+                input_accelerations,
+                training_prediction="mixed_teacher_forcing",
+                position_seq_target=output_bboxes,
+                velocity_seq_target=output_velocities,
+                teacher_forcing_ratio=0.6,
+            )
+        else:
+            predicted_positions, predicted_velocity = self(
+                input_bboxes, input_velocities, input_accelerations
+            )
+
+        # Convert predicted future velocities to future positions
+        velocities_to_positions = convert_velocity_to_positions(
+            predicted_velocity=predicted_velocity,
+            past_positions=input_bboxes,
+        )
+
+        # Compute losses
+        velocity_loss = F.smooth_l1_loss(predicted_velocity, output_velocities)
+        pos_loss = F.smooth_l1_loss(predicted_positions, output_bboxes)
+        velocities_to_positions_loss = F.smooth_l1_loss(
+            velocities_to_positions, output_bboxes
+        )
+        total_loss = velocity_loss + velocities_to_positions_loss * 0.1 + pos_loss
+
+        # Compute metrics
+        ade_from_vel = compute_ADE(
+            velocities_to_positions, output_bboxes, self.hparams.image_size
+        )
+        fde_from_vel = compute_FDE(
+            velocities_to_positions, output_bboxes, self.hparams.image_size
+        )
+        aiou_from_vel = compute_AIOU(velocities_to_positions, output_bboxes)
+        fiou_from_vel = compute_FIOU(velocities_to_positions, output_bboxes)
+
+        ade = compute_ADE(predicted_positions, output_bboxes, self.hparams.image_size)
+        fde = compute_FDE(predicted_positions, output_bboxes, self.hparams.image_size)
+        aiou = compute_AIOU(predicted_positions, output_bboxes)
+        fiou = compute_FIOU(predicted_positions, output_bboxes)
+
+        # Log metrics
+        self.log(
+            f"{stage}_loss", total_loss, on_step=False, on_epoch=True, prog_bar=False
+        )
+        self.log(f"{stage}_ADE", ade, on_step=False, on_epoch=True, prog_bar=False)
+        self.log(f"{stage}_FDE", fde, on_step=False, on_epoch=True, prog_bar=False)
+        self.log(
+            f"{stage}_AIoU",
+            aiou,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=False if stage == "train" else True,
+        )
+        self.log(f"{stage}_FIoU", fiou, on_step=False, on_epoch=True, prog_bar=True)
+        self.log(
+            f"{stage}_ADE_from_vel",
+            ade_from_vel,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=False,
+        )
+        self.log(
+            f"{stage}_FDE_from_vel",
+            fde_from_vel,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=False,
+        )
+        self.log(
+            f"{stage}_AIoU_from_vel",
+            aiou_from_vel,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=False if stage == "train" else True,
+        )
+        self.log(
+            f"{stage}_FIoU_from_vel",
+            fiou_from_vel,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+        )
+
+        return {
+            "total_loss": total_loss,
+            "velocity_loss": velocity_loss,
+            "velocities_to_positions_loss": velocities_to_positions_loss,
+            "ADE": ade,
+            "FDE": fde,
+            "AIoU": aiou,
+            "FIoU": fiou,
+        }
+
+    def training_step(self, batch, batch_idx):
+        return self._shared_step(batch, batch_idx, "train")["total_loss"]
+
+    def validation_step(self, batch, batch_idx):
+        return self._shared_step(batch, batch_idx, "val")
+
+    def test_step(self, batch, batch_idx):
+        return self._shared_step(batch, batch_idx, "test")
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer,
+            mode="min",
+            patience=self.hparams.scheduler_patience,
+            factor=self.hparams.scheduler_factor,
+            threshold=1e-8,
+        )
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "monitor": "val_loss",
+                "interval": "epoch",
+            },
+        }
